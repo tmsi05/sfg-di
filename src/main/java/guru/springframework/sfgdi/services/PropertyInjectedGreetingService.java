2a56ef57b369d6edc169c697728985c005c8b032
Antonio Afonso <tmsimk@gmail.com>
2024-03-22 15:07:50 +0000

    After follow-up commit

--- src/main/java/guru/springframework/sfgdi/services/PropertyInjectedGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PropertyInjectedGreetingService.java
@@ -2,13 +2,10 @@ package guru.springframework.sfgdi.services;
 
 import org.springframework.stereotype.Service;
 
-/**
- * Created by jt on 12/26/19.
- */
 @Service
-public class GreetingServiceImpl implements GreetingService {
+public class PropertyInjectedGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello World";
+        return "Hello World - Property";
     }
 }
