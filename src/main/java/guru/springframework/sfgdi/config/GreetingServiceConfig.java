3112e57ff30079e3518c4061ba28da832bc73cef
Antonio Afonso <tmsimk@gmail.com>
2024-04-03 17:10:45 +0100

    committing component-scan-follow-along

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,16 +1,41 @@
 package guru.springframework.sfgdi.config;
 
-import guru.springframework.sfgdi.services.ConstructorGreetingService;
-import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
-import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 /**
  * Created by jt on 2/20/21.
  */
 @Configuration
 public class GreetingServiceConfig {
+
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService  i18nService(){
+        return new I18nEnglishGreetingService();
+    }
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")    // Para equivaler ao memso nome de beando seguinte
+                            // By default spring uses the method name for the bean
+                            // Unless you override it in the bean annotation
+                            // and since you cannot have the same method declared twice in the same class
+                            // we have to declare the name of the bean to use the same name
+    I18NSpanishService i18NSpanishService() {
+        return new I18NSpanishService();
+    }
+
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
+
 
     @Bean
     ConstructorGreetingService constructorGreetingService(){
